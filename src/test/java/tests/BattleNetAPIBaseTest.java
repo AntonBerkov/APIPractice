4d40e7e07cf38aaa5b9ad9d769e94eff79a3de0a
AntonBerkov <aberkov505@gmail.com>
2023-04-27 17:03:19 +0200

    Added report portal tests

--- src/test/java/tests/BattleNetAPIBaseTest.java
+++ src/test/java/tests/BattleNetAPIBaseTest.java
@@ -11,7 +11,7 @@ public class BattleNetAPIBaseTest {
 
     public RequestSpecification initRequest() {
         String token = given().relaxedHTTPSValidation()
-                .header(HttpHeaders.AUTHORIZATION, Services.getProperty("auth.token"))
+                .header(HttpHeaders.AUTHORIZATION, Services.getProperty("authbattle.token"))
                 .param("grant_type", "client_credentials").when()
                 .post("https://oauth.battle.net/token").then().statusCode(HttpStatus.SC_OK).extract().body().path("access_token");
         baseURI = "https://eu.api.blizzard.com";
