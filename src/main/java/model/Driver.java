5c6fed3d172e46af0164cb16a3e3a32397c093c5
AntonBerkov <aberkov505@gmail.com>
2023-07-20 14:18:27 +0200

    Deleted chromedriver

--- src/main/java/model/Driver.java
+++ src/main/java/model/Driver.java
@@ -11,7 +11,6 @@ public class Driver {
     }
 
     public static WebDriver getDriver() {
-        System.setProperty("webdriver.chrome.driver", "https://github.com/AntonBerkov/GoogleTranslateAPIPractice/blob/6fe9d13a20b602c6a53d82846c6fac3133bd76a4/src/test/resources/drivers/chromedriver.exe");
         if (driver == null) {
             driver = new ChromeDriver();
             driver.manage().window().maximize();
